5bd811ea62cd6015a2c94fb6e1eb0f469ece9faf
juanirealrulo <juani.realrulo@gmail.com>
2026-07-01 19:10:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function crearLeadEnAirtable(datos) {
 
   fetch(WEBHOOK_URL, {
     method: 'POST',
+    mode: 'no-cors',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
   }).catch(function (error) {
